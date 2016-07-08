05f847aec3b8e30af0dc04d7ee74880a33b77d0a
Vicky Gupta <vckgupta008@gmail.com>
2016-07-08 16:47:24 +0530

    first commit

---
+++ includes/js/script.js
@@ -0,0 +1,48 @@
+/*
+var pagetop,menu,yPos;
+function yScroll(){
+	pagetop=document.getElementById('pagetop');
+	menu=document.getElementById('menu');
+	yPos=window.pageYOffset;
+	if(yPos > 150)
+	{
+		pagetop.style.height="36px";
+		pagetop.style.paddingTop="8px";
+		menu.style.height="0px";
+	}
+	else
+	{
+		pagetop.style.height="120px";
+		pagetop.style.paddingTop="50px";
+		menu.style.height="50px";
+	}
+}
+window.addEventListener("scroll",yScroll);*/
+
+/*
+var pagetop,logo,yPos;
+function yScroll(){
+	pagetop=document.getElementById('head');
+	pagetop1=document.getElementById('head1');
+	logo=document.getElementById('logo');
+	yPos=window.pageYOffset;
+	if(yPos > 150)
+	{
+		pagetop.style.height="50px";
+		pagetop.style.paddingTop="0px";
+		logo.style.height="50px";
+		logo.style.paddingTop="0px";
+		pagetop1.style.height="50px";
+		
+	}
+	else
+	{
+		pagetop.style.height="120px";
+		pagetop.style.paddingTop="50px";
+		pagetop1.style.height="100px";
+		pagetop1.style.paddingTop="20px";
+		pagetop1.style.margingTop="70px";
+	}
+}
+window.addEventListener("scroll",yScroll);
+*/
