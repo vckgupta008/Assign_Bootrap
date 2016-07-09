d4268f0d7eaf0d6530db38f7d56df1c918bc3129
vckgupta008 <vckgupta008@gmail.com>
2016-07-09 23:14:38 +0530

    Commit

--- includes/js/script.js
+++ includes/js/script.js
@@ -1,48 +1,18 @@
-/*
-var pagetop,menu,yPos;
-function yScroll(){
-	pagetop=document.getElementById('pagetop');
-	menu=document.getElementById('menu');
-	yPos=window.pageYOffset;
-	if(yPos > 150)
-	{
-		pagetop.style.height="36px";
-		pagetop.style.paddingTop="8px";
-		menu.style.height="0px";
-	}
-	else
-	{
-		pagetop.style.height="120px";
-		pagetop.style.paddingTop="50px";
-		menu.style.height="50px";
-	}
-}
-window.addEventListener("scroll",yScroll);*/
+$(function(){
 
-/*
-var pagetop,logo,yPos;
-function yScroll(){
-	pagetop=document.getElementById('head');
-	pagetop1=document.getElementById('head1');
-	logo=document.getElementById('logo');
-	yPos=window.pageYOffset;
-	if(yPos > 150)
+	$('#alertMe').click(function(e)
 	{
-		pagetop.style.height="50px";
-		pagetop.style.paddingTop="0px";
-		logo.style.height="50px";
-		logo.style.paddingTop="0px";
-		pagetop1.style.height="50px";
-		
-	}
-	else
-	{
-		pagetop.style.height="120px";
-		pagetop.style.paddingTop="50px";
-		pagetop1.style.height="100px";
-		pagetop1.style.paddingTop="20px";
-		pagetop1.style.margingTop="70px";
-	}
-}
-window.addEventListener("scroll",yScroll);
-*/
+			e.preventDefault();
+			$('#successAlert').slideDown();
+	});
+
+	$('a.pop').click(function(e) {
+		e.preventDefault();
+	});
+	
+	$('a.pop').popover();
+	
+	$('[rel="tooltip"]').tooltip();
+	
+
+	});
